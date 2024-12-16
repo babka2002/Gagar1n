23f2db4c3eccf79d9f38da6aa05bc807d66d1f09
pvio <pvio@mail.me>
2024-12-16 20:48:29 +0300

    16-12-2024 20:48:29

--- html_slice/vite.config.js
+++ html_slice/vite.config.js
@@ -1,12 +1,22 @@
-import { defineConfig } from 'vite'
-import { sync as globSync } from 'glob'
+import nunjucks from 'vite-plugin-nunjucks'
 
-const htmlFiles = globSync('**/*.html');
+export default {
+    plugins: [
+        nunjucks(),
+    ]
+}
 
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: htmlFiles
-    }
-  }
-})
+
+
+// import { defineConfig } from 'vite'
+// import { sync as globSync } from 'glob'
+//
+// const htmlFiles = globSync('**/*.html');
+//
+// export default defineConfig({
+//   build: {
+//     rollupOptions: {
+//       input: htmlFiles
+//     }
+//   }
+// })
