5bf6b16313f529d3d204c140c9fb0736f42639c6
Evgenii Zhukov <evgenii.z.i@yandex.ru>
2024-12-29 17:22:18 +0300

    Add ru location

---
+++ lang/ru/date.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'days' => [
+        'Monday' => 'Понедельник',
+        'Tuesday' => 'Вторник',
+        'Wednesday' => 'Среда',
+        'Thursday' => 'Четверг',
+        'Friday' => 'Пятница',
+        'Saturday' => 'Суббота',
+        'Sunday' => 'Воскресенье'
+    ]
+];
