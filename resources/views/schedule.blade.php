447cde98be6503ebe4fbef3c30313bc98c65b18f
Evgenii Zhukov <evgenii.z.i@yandex.ru>
2025-02-04 12:59:08 +0300

    fix

--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -14,11 +14,17 @@
 
                 <!-- Кнопка индивидуальных тренировок -->
                 <a
-                    href="{{ route('schedule.show', array_merge(request()->except('schedule_type'), ['schedule_type' => 'personal'])) }}"
+                    href="/personal-training"
                     class="rounded-brxl bg-main-red w-1/2 py-4 md:py-12 px-4 flex items-center justify-center text-center text-light text-sm md:text-md uppercase transition-all hover:scale-90 {{ $scheduleType === 'personal' ? 'opacity-100' : 'opacity-70' }}"
                 >
                     <span>Индивидуальные тренировки</span>
                 </a>
+                {{-- <a
+                    href="{{ route('schedule.show', array_merge(request()->except('schedule_type'), ['schedule_type' => 'personal'])) }}"
+                    class="rounded-brxl bg-main-red w-1/2 py-4 md:py-12 px-4 flex items-center justify-center text-center text-light text-sm md:text-md uppercase transition-all hover:scale-90 {{ $scheduleType === 'personal' ? 'opacity-100' : 'opacity-70' }}"
+                >
+                    <span>Индивидуальные тренировки</span>
+                </a> --}}
             </div>
             <div class="col-span-12 md:col-span-3">
                 <a
