d948da9d1e75a9a7de44f30265f852780d415213
Evgenii Zhukov <evgenii.z.i@yandex.ru>
2025-02-04 12:56:47 +0300

    fix

--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -22,7 +22,7 @@
             </div>
             <div class="col-span-12 md:col-span-3">
                 <a
-                    href="#"
+                    href="/gruppovye-trenirovki"
                     class="rounded-brxl bg-main-red w-full py-2 px-4 flex items-center justify-center text-center text-light text-sm md:text-md uppercase transition-all hover:scale-90"
                 >
                     <span class="mr-2"> описание групповых тренировок </span>
