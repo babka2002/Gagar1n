4d2fb7fd0f34b238626a5acca3b1715b72344819
Evgenii Zhukov <evgenii.z.i@yandex.ru>
2025-02-09 17:25:41 +0300

    fix - add sites config

---
+++ config/statamic/sites.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'sites' => [
+        'Grelka' => [
+            'name' => 'grelka',
+            'layout' => 'grelka/layout',
+            'url' => '/',
+            'locale' => 'ru_RU',
+            'lang' => 'ru'
+        ],
+        'Gun1or' => [
+            'name' => 'gun1or',
+            'layout' => 'gun1or/layout',
+            'url' => '/gun1or/',
+            'locale' => 'ru_RU',
+            'lang' => 'ru'
+        ],
+        'GFoodcafe' => [
+            'name' => 'gfoodcafe',
+            'layout' => 'gfoodcafe/layout',
+            'url' => '/gfoodcafe/',
+            'locale' => 'ru_RU',
+            'lang' => 'ru'
+        ]
+    ]
+];
