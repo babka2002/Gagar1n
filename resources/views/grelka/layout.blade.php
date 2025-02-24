c7871be7ff75c932298c947ea1520574d84a0c97
Evgenii Zhukov <evgenii.z.i@yandex.ru>
2025-02-24 14:25:00 +0300

    fix

--- resources/views/grelka/layout.blade.php
+++ resources/views/grelka/layout.blade.php
@@ -35,7 +35,15 @@
         <link href="{{ asset('vendor/bladewind/css/bladewind-ui.min.css') }}" rel="stylesheet" />
         <script src="{{ asset('vendor/bladewind/js/helpers.js') }}"></script> --}}
 
-        @vite(['resources/css/site.css', 'resources/css/markdown-content.css', 'resources/js/site.js'])
+        {{-- @vite(['resources/css/site.css', 'resources/css/markdown-content.css', 'resources/js/site.js']) --}}
+        @vite([
+            'resources/css/site.css',
+            'resources/css/markdown-content.css',
+            'resources/js/site.js',
+            'vendor/mkocansey/bladewind/public/css/animate.min.css',
+            'vendor/mkocansey/bladewind/public/css/bladewind-ui.min.css',
+            'vendor/mkocansey/bladewind/public/js/helpers.js'
+        ])
         <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     </head>
     <body class="bg-dark">
